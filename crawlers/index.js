0fadeb752284aaf2822a892318bf937069b4b33f
Colin <qingyu.wang@aliyun.com>
2019-06-08 15:40:13 +0800

    :sparkles: (backend) add router for super index of weibo

--- crawlers/index.js
+++ crawlers/index.js
@@ -1,5 +1,20 @@
 const superIndex = require('./routes/weibo/superIndex');
+const express = require('express');
+const app = express();
 
-module.exports = {
-  superIndex,
-};
+const port = 3000;
+
+app.get('/weibo/super_index/:id', (req, res) => {
+  let { id } = req.params;
+  superIndex(id).then(data => {
+    res.send(data);
+  });
+});
+
+app.get('/weibo/super_index', (req, res) => {
+  superIndex().then(data => {
+    res.send(data);
+  });
+});
+
+app.listen(port, () => console.log(`running at ${port}`));
