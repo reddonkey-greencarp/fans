59038eccebcf93b7c66348a0bb5e17380d1a95fb
Colin <qingyu.wang@aliyun.com>
2019-06-08 23:15:20 +0800

    :sparkles: add handlers to router

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/reddonkey-greencarp/fans/backend/handlers"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, "pong")
 	})
+	r.POST("/schedule", handlers.AddSchedule)
+	r.GET("/schedule", handlers.Schedule)
+	r.POST("/offline", handlers.AddOffline)
+	r.GET("/offline", handlers.Offline)
+	r.POST("/question", handlers.AddQuestion)
+	r.GET("/question", handlers.Question)
 	r.Run(":8000")
 }
