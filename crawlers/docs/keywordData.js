dd99f81495c3403f43ec1fcec99736e1a4a62edb
Colin <qingyu.wang@aliyun.com>
2019-06-08 17:40:43 +0800

    :memo: (weibo) add docs for super index and keyword

---
+++ crawlers/docs/keywordData.js
@@ -0,0 +1,82 @@
+let data = {
+  title: '又有人在微博提到杨超越了',
+  link: 'http://s.weibo.com/weibo/%E6%9D%A8%E8%B6%85%E8%B6%8A&b=1&nodup=1',
+  item: [
+    {
+      createdAt: '刚刚',
+      content:
+        '<a  href="https://m.weibo.cn/search?containerid=231522type%3D1%26t%3D10%26q%3D%23%E6%88%91%E4%B8%BA%E8%B6%85%E8%B6%8A%E7%8E%A9%E5%A4%A7%E8%AF%9D%23&extparam=%23%E6%88%91%E4%B8%BA%E8%B6%85%E8%B6%8A%E7%8E%A9%E5%A4%A7%E8%AF%9D%23&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">#我为超越玩大话#</span></a> <a  href="https://m.weibo.cn/search?containerid=231522type%3D1%26t%3D10%26q%3D%23%E6%9D%A8%E8%B6%85%E8%B6%8A%E4%BB%A3%E8%A8%80%E5%A4%A7%E8%AF%9D%E8%A5%BF%E6%B8%B8%E6%89%8B%E6%B8%B8%23&extparam=%23%E6%9D%A8%E8%B6%85%E8%B6%8A%E4%BB%A3%E8%A8%80%E5%A4%A7%E8%AF%9D%E8%A5%BF%E6%B8%B8%E6%89%8B%E6%B8%B8%23&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">#杨超越代言大话西游手游#</span></a>  加油！转发 <a href="https://weibo.com/1323527941" target="_blank">@Vista看天下</a>: 【一年了，杨超越凭什么还能火？】不知不觉一年过去了，101女孩中最能创造话题的，竟然还是杨超越。<br><br>曾经人们都担心她的唱跳能力会拖后腿，但好在成团后杨超越在公演上的表现倒也没明显被吊打。反倒是参加的几个综艺节目，让她收获了不少热度。<br> <br>或者说，与正统的女爱豆相比，杨超越显得更加“出圈”。即便是不了解女团，甚至不熟悉娱乐圈的普通人，也能熟知杨超越的二三事。<br><br>当杨超越一年之后还在火，我们不得不正视这个被认为“躺赢”的女孩：为什么会是她？为什么总是她？<br>#杨超越代言大话西游手游#',
+      pic: null,
+      comments: 0,
+      link: 'https://weibo.com/3006694604/HxYf5w2Yn',
+    },
+    {
+      createdAt: '1分钟前',
+      content:
+        '<a  href="https://m.weibo.cn/search?containerid=231522type%3D1%26t%3D10%26q%3D%23%E6%9D%A8%E8%B6%85%E8%B6%8A%23&isnewpage=1&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">#杨超越#</span></a> 欠扁呗，明日之女这么大个s级资源都要开始了，非要挑事。这种属于大龙快刷了非要装逼，被团灭还丢龙丢高地 ',
+      pic: null,
+      comments: 0,
+      link: 'https://weibo.com/3227630873/HxYeVefoK',
+    },
+    {
+      createdAt: '1分钟前',
+      content: `<a  href="https://m.weibo.cn/p/index?containerid=100808a474470abf426386e53da6f086424098&extparam=%E7%81%AB%E7%AE%AD%E5%B0%91%E5%A5%B3101&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">火箭少女101</span></a> <a  href="https://m.weibo.cn/p/index?containerid=1008082a98366b6a3546bd16e9da0571e34b84&extparam=%E6%9D%A8%E8%B6%85%E8%B6%8A&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">杨超越</span></a> <a  href="https://m.weibo.cn/p/index?containerid=10080828a9bf1b7bd295c340456529fd8b5a4c&extparam=%E6%AE%B5%E5%A5%A5%E5%A8%9F&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">段奥娟</span></a> <a  href="https://m.weibo.cn/p/index?containerid=10080872c2b55ff59d9e1d9b520733b1d64edc&extparam=%E8%B5%96%E7%BE%8E%E4%BA%91&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">赖美云</span></a> 锁定今晚九点半，一起来看妹妹们吧[好喜歡][好喜歡][好喜歡]//<a href='/n/火箭少女101官博'>@火箭少女101官博</a>:<a  href="https://m.weibo.cn/p/index?containerid=100808a474470abf426386e53da6f086424098&extparam=%E7%81%AB%E7%AE%AD%E5%B0%91%E5%A5%B3101&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">火箭少女101</span></a> <a  href="https://m.weibo.cn/p/index?containerid=1008082a98366b6a3546bd16e9da0571e34b84&extparam=%E6%9D%A8%E8%B6%85%E8%B6%8A&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">杨超越</span></a> <a  href="https://m.weibo.cn/p/index?containerid=10080828a9bf1b7bd295c340456529fd8b5a4c&extparam=%E6%AE%B5%E5%A5%A5%E5%A8%9F&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">段奥娟</span></a> <a  href="https://m.weibo.cn/p/index?containerid=10080872c2b55ff59d9e1d9b520733b1d64edc&extparam=%E8%B5%96%E7%BE%8E%E4%BA%91&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">赖美云</span></a> <a data-url="http://t.cn/AiCUrhIF" href="http://m.weibo.cn/client/version?luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">今晚21:30深圳卫视</span></a>  <a  href="https://m.weibo.cn/p/index?containerid=1008084aca80448da5859d3b1a240f67d878b9&extparam=%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">非常静距离</span></a> 等你哦转发 <a href="https://weibo.com/1563848232" target="_blank">@非常静距离JING</a>: <a href='/n/火箭少女101_杨超越'>@火箭少女101_杨超越</a>  <a href='/n/火箭少女101_段奥娟'>@火箭少女101_段奥娟</a>  <a href='/n/火箭少女101_赖美云'>@火箭少女101_赖美云</a>  <br /><a  href="https://m.weibo.cn/search?containerid=231522type%3D1%26t%3D10%26q%3D%23%E6%9D%A8%E8%B6%85%E8%B6%8A%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB%23&extparam=%23%E6%9D%A8%E8%B6%85%E8%B6%8A%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB%23&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">#杨超越非常静距离#</span></a><a  href="https://m.weibo.cn/search?containerid=231522type%3D1%26t%3D10%26q%3D%23%E6%AE%B5%E5%A5%A5%E5%A8%9F%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB%23&extparam=%23%E6%AE%B5%E5%A5%A5%E5%A8%9F%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB%23&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">#段奥娟非常静距离#</span></a><a  href="https://m.weibo.cn/search?containerid=231522type%3D1%26t%3D10%26q%3D%23%E8%B5%96%E7%BE%8E%E4%BA%91%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB%23&extparam=%23%E8%B5%96%E7%BE%8E%E4%BA%91%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB%23&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">#赖美云非常静距离#</span></a>  <br />挨打怎么办？我忍 实在不行就互捶！<br />难忘的大胆骑自行车经历、爬树、捉蝌蚪、臭水沟摸鱼......<br />今晚21:30<a href='/n/深圳卫视'>@深圳卫视</a> <a  href="https://m.weibo.cn/p/index?containerid=1008084aca80448da5859d3b1a240f67d878b9&extparam=%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">非常静距离</span></a> <a data-url="http://t.cn/AiChR7Ic" href="https://m.weibo.cn/p/index?containerid=2304444380574480047667&url_type=39&object_type=video&pos=1&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">非常静距离JING的微博视频</span></a> `,
+      pic: null,
+      comments: 0,
+      link: 'https://weibo.com/6580801659/HxYeLrgkY',
+    },
+    {
+      createdAt: '1分钟前',
+      content:
+        '<a  href="https://m.weibo.cn/p/index?containerid=1008082a98366b6a3546bd16e9da0571e34b84&extparam=%E6%9D%A8%E8%B6%85%E8%B6%8A&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">杨超越</span></a>哈哈哈哈哈哈哈哈哈嗝好他妈开心 考完啦我的越越麻麻来啦啦咯啦咯啦咯啦咯啦咯 ',
+      pic: null,
+      comments: 0,
+      link: 'https://weibo.com/6568009558/HxYeG9aBK',
+    },
+    {
+      createdAt: '1分钟前',
+      content:
+        '发布了头条文章：《杨超越出道一周年：从自卑到自信，只因她做到了这三点》  <a data-url="http://t.cn/AiCUkjOT" href="https://media.weibo.cn/article?object_id=1022%3A2309404380963709838784&extparam=lmid--4380963712662463&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0&id=2309404380963709838784" data-hide=""><span class="surl-text">杨超越出道一周年：从自卑到自信，只因她做到了这三点</span></a> ',
+      pic: null,
+      comments: 0,
+      link: 'https://weibo.com/3028976864/HxYeDbaCX',
+    },
+    {
+      createdAt: '2分钟前',
+      content:
+        '<a  href="https://m.weibo.cn/p/index?containerid=1008082a98366b6a3546bd16e9da0571e34b84&extparam=%E6%9D%A8%E8%B6%85%E8%B6%8A&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">杨超越</span></a>考完了，恭喜自己！哈哈哈哈哈哈啊哈哈哈哈哈哈哈哈哈哈哈😄 ！ ',
+      pic: 'https://wx4.sinaimg.cn/large/006oxX8sgy1g3tvtsnkcmj30u018yn5e.jpg',
+      comments: 1,
+      link: 'https://weibo.com/5859521200/HxYeqaS6z',
+    },
+    {
+      createdAt: '2分钟前',
+      content: `CCYY//<a href='/n/CCYY-ZZ'>@CCYY-ZZ</a>:掐指一算，杨超越要火101年，还有100年 💜一年的时光把璞玉雕琢成钻石💜杨超越手握四个个人代言💜准一线男刊实体刊💜两个个人奖项💜女明星杂志销量第一💜期待杨超越录制的《非常静距离》和《快乐大本营》💜出演的《羽你同行》以及古装剧《长安诺》和《将夜2》💜转发 <a href="https://weibo.com/1323527941" target="_blank">@Vista看天下</a>: 【一年了，杨超越凭什么还能火？】不知不觉一年过去了，101女孩中最能创造话题的，竟然还是杨超越。<br><br>曾经人们都担心她的唱跳能力会拖后腿，但好在成团后杨超越在公演上的表现倒也没明显被吊打。反倒是参加的几个综艺节目，让她收获了不少热度。<br> <br>或者说，与正统的女爱豆相比，杨超越显得更加“出圈”。即便是不了解女团，甚至不熟悉娱乐圈的普通人，也能熟知杨超越的二三事。<br><br>当杨超越一年之后还在火，我们不得不正视这个被认为“躺赢”的女孩：为什么会是她？为什么总是她？<br>#杨超越代言大话西游手游#`,
+      pic: null,
+      comments: 0,
+      link: 'https://weibo.com/1102224383/HxYepBSla',
+    },
+    {
+      createdAt: '2分钟前',
+      content: `哈哈哈，太欢乐了，太期待今天晚上的正式节目了。转发 <a href="https://weibo.com/1563848232" target="_blank">@非常静距离JING</a>: <a href='/n/火箭少女101_杨超越'>@火箭少女101_杨超越</a>  <a href='/n/火箭少女101_段奥娟'>@火箭少女101_段奥娟</a>  <a href='/n/火箭少女101_赖美云'>@火箭少女101_赖美云</a>  <br /><a  href="https://m.weibo.cn/search?containerid=231522type%3D1%26t%3D10%26q%3D%23%E6%9D%A8%E8%B6%85%E8%B6%8A%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB%23&extparam=%23%E6%9D%A8%E8%B6%85%E8%B6%8A%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB%23&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">#杨超越非常静距离#</span></a><a  href="https://m.weibo.cn/search?containerid=231522type%3D1%26t%3D10%26q%3D%23%E6%AE%B5%E5%A5%A5%E5%A8%9F%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB%23&extparam=%23%E6%AE%B5%E5%A5%A5%E5%A8%9F%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB%23&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">#段奥娟非常静距离#</span></a><a  href="https://m.weibo.cn/search?containerid=231522type%3D1%26t%3D10%26q%3D%23%E8%B5%96%E7%BE%8E%E4%BA%91%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB%23&extparam=%23%E8%B5%96%E7%BE%8E%E4%BA%91%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB%23&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">#赖美云非常静距离#</span></a>  <br />挨打怎么办？我忍 实在不行就互捶！<br />难忘的大胆骑自行车经历、爬树、捉蝌蚪、臭水沟摸鱼......<br />今晚21:30<a href='/n/深圳卫视'>@深圳卫视</a> <a  href="https://m.weibo.cn/p/index?containerid=1008084aca80448da5859d3b1a240f67d878b9&extparam=%E9%9D%9E%E5%B8%B8%E9%9D%99%E8%B7%9D%E7%A6%BB&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">非常静距离</span></a> <a data-url="http://t.cn/AiChR7Ic" href="https://m.weibo.cn/p/index?containerid=2304444380574480047667&url_type=39&object_type=video&pos=1&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">非常静距离JING的微博视频</span></a> `,
+      pic: null,
+      comments: 0,
+      link: 'https://weibo.com/1058880813/HxYeivoEL',
+    },
+    {
+      createdAt: '3分钟前',
+      content:
+        '转发这个为你打call的杨超越，不会的题都蒙对，高考无压力转发 <a href="https://weibo.com/5644764907" target="_blank">@火箭少女101_杨超越</a>: <a  href="https://m.weibo.cn/search?containerid=231522type%3D1%26t%3D10%26q%3D%23%E7%AB%AF%E5%8D%88%E8%8A%82%E8%A6%81%E8%BF%99%E6%A0%B7%E8%BF%87%23&extparam=%23%E7%AB%AF%E5%8D%88%E8%8A%82%E8%A6%81%E8%BF%99%E6%A0%B7%E8%BF%87%23&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">#端午节要这样过#</span></a> 端午快乐～帮我尝尝粽子的味道～祝大家高考顺利加油⛽️❤️ ',
+      pic: null,
+      comments: 0,
+      link: 'https://weibo.com/2213260331/HxYe7wMqo',
+    },
+    {
+      createdAt: '3分钟前',
+      content: `杨超越粉丝应援给力//<a href='/n/zhei是高手'>@zhei是高手</a>:转发微博转发 <a href="https://weibo.com/5022912024" target="_blank">@夹子居居ii</a>: <a  href="https://m.weibo.cn/p/index?containerid=1008082a98366b6a3546bd16e9da0571e34b84&extparam=%E6%9D%A8%E8%B6%85%E8%B6%8A&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">杨超越</span></a>村村月芽今天都很辛苦！都是战士！超越我们进不去，在40棚场外的呐喊你听到了吗！！！<a href='/n/火箭少女101_杨超越'>@火箭少女101_杨超越</a> <a data-url="http://t.cn/AiCUDGBm" href="https://m.weibo.cn/p/index?containerid=2304444380962545677144&url_type=39&object_type=video&pos=1&luicode=10000011&lfid=100103type%253D61%2526q%253D%25E6%259D%25A8%25E8%25B6%2585%25E8%25B6%258A%2526t%253D0" data-hide=""><span class="surl-text">夹子居居ii的秒拍视频</span></a> `,
+      pic: null,
+      comments: 0,
+      link: 'https://weibo.com/2822754913/HxYe64bAU',
+    },
+  ],
+};
