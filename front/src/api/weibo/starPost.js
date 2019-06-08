64c677e25205bdf040315f38d4ab7d4aa9d4549f
Colin <qingyu.wang@aliyun.com>
2019-06-09 03:06:56 +0800

    :sparkles: (api) add starPost api
    for getting some star's posts

---
+++ front/src/api/weibo/starPost.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const { NodeHost } = require("../../config/const");
+
+/**
+ * @typedef Post
+ * @type {Object}
+ * @property {string} title - the simplest content (without emoji and faces)
+ * @property {string} createdAt - the create time, usually "刚刚" or "x分钟前"
+ * @property {string} content - the content of post (may be null)
+ * @property {string} pic - the url of the pic if has (null for none)
+ * @property {number} comments - the number of comments
+ * @property {number} likes - the numbers of likes
+ */
+
+/**
+ * @typedef StarRes
+ * @type {Object}
+ * @property {string} title - the title that can be directly displayed
+ * @property {string} link - the link to the star's weibo
+ * @property {string} image - the avatar of the star
+ * @property {Post[]} items - the post of the star
+ */
+
+/**
+ * @returns {Promise.<StarRes>}
+ */
+module.exports = async () => {
+  try {
+    let resp = await axios.get(NodeHost + "/weibo/star");
+    return resp.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
